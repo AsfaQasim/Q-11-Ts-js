4414544e5b685ec63430ffee0511532ae36df206
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:33:48 -0800

    Q 11 of Ts/js & node js of 45 assignment
    Q 11 of Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,4 @@
+var members = ["Faria", "Sundas", "Iqra", "Nazish"];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+ let members : string[] =[`Faria`,`Sundas`,`Iqra`,`Nazish`];
+ for(let i=0; i<members.length; i++)
+ {
+     console.log(members[i]);
+ }
